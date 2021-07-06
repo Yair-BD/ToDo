d9f62c5a270986119f6cdba8b1d95a0b7a7522b3
Yair <yair2321@gmail.com>
2021-07-06 10:40:17 +0300

    added timefield

--- tasks/models.py
+++ tasks/models.py
@@ -8,10 +8,9 @@ from django.contrib.auth.models import User
 class Task(models.Model):
     title = models.CharField(max_length=200, null=True)
     complete = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True, auto_now=False, blank=True)
     updated_at = models.DateTimeField(auto_now_add=False, auto_now=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, auto_now=False, blank=True)
+
 
     def __str__(self):
         return self.title
-
-   
